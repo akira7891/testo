58f132ffb450038bc07576b754642ce2c7db6285
akira <akiraakira462@gmail.com>
2022-05-17 19:35:26 +0900

    シーディング完了

---
+++ database/seeders/TweetsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TweetsSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('tweets')->insert(['content'=>Str::random(10),'created_at'=>now(10),'updated_at'=>now(),]);
+    }
+}
